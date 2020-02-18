c3e8a868d6dfa21eaac06084adc5ab3b5e06233e
Spencer Couture <spence.couture@gmail.com>
2020-02-18 18:15:29 -0500

    Uses OpenCV kalman library

--- main/main.cpp
+++ main/main.cpp
@@ -4,14 +4,17 @@
 double stateMatrixArr[] = {0, 0, 0, 0};
 cv::Mat stateMatrix( cv::Size(1, 4), CV_64F, stateMatrixArr );
 
-double hArr[] = {1,0,1,0};
-cv::Mat h( cv::Size(4, 1), CV_64F, hArr);
+double hArr[2][4] = {
+    {1, 0, 0, 0},
+    {0, 0, 1, 0}
+};
+cv::Mat h( cv::Size(4, 2), CV_64F, hArr);
 
 double procCovarArr[4][4] = {
-    {0.2, 0, 0, 0},
-    {0, 0.2, 0, 0},
-    {0, 0, 0.2, 0},
-    {0, 0, 0, 0.2}
+    {0, 0.001, 0, 0},
+    {0.001, 0.001, 0, 0},
+    {0, 0, 0, 0.001},
+    {0, 0, 0.001, 0.001}
 };
 cv::Mat procCovar( cv::Size(4, 4), CV_64F, procCovarArr );
 
@@ -25,23 +28,35 @@ double stateTransArr[4][4] = {
 cv::Mat stateTrans( cv::Size(4, 4), CV_64F, stateTransArr);
 
 double pArr[4][4] = {
-    {0, 0, 0, 0},
-    {0, 0, 0, 0},
-    {0, 0, 0, 0},
-    {0, 0, 0, 0}
+    {10, 0, 0, 0},
+    {0, 500, 0, 0},
+    {0, 0, 10, 0},
+    {0, 0, 0, 500}
 };
 cv::Mat p( cv::Size(4, 4), CV_64F, pArr);
 
-double obsCovarArr[] = {64,0, 64,0};
-cv::Mat obsCovar( cv::Size(1, 4), CV_64F, obsCovarArr);
+double obsCovarArr[2][2] = {
+    {64,0},
+    {0,64}
+};
+cv::Mat obsCovar( cv::Size(2, 2), CV_64F, obsCovarArr);
 
 cv::Mat gain;
 
-cv::Vec3f kalman( cv::Vec3f ball, std::chrono::microseconds delta){
+
+
+
+
+
+cv::Vec3f oldkalman( cv::Vec3f ball, std::chrono::microseconds delta){
     
+
+    /*
     stateMatrix.at<double>(0, 0) = ball[0];
     stateMatrix.at<double>(0, 2) = ball[1];
     // set ball velocities here too   
+
+    if( delta.count() == 0 ) return NULL;
 
     // set delta values in stateTrans
     stateTrans.at<double>(0, 1) = delta.count();
@@ -53,31 +68,63 @@ cv::Vec3f kalman( cv::Vec3f ball, std::chrono::microseconds delta){
     cv::Mat smc = h.t();
     std::cout << "t: " << std::endl << smc << std::endl;
 
-    cv::Mat tmp = p * h.t();
+    /*cv::Mat tmp = p * h.t();
     std::cout << "abc1" << std::endl;
     cv::Mat tmp2 = (h*p*h.t());
     std::cout << "abc2" << std::endl;
     tmp2 = tmp2 + obsCovar;
     std::cout << "abc3" << std::endl;
-    tmp = tmp * tmp2;
-    std::cout << "abc4" << std::endl;
-    gain = p * h.t() * (h*p*h.t() + obsCovar);
-    std::cout << "SMC: " << std::endl << tmp << std::endl;
-    gain = tmp.inv();
+    tmp = tmp * tmp2.inv();
+    std::cout << "abc4" << std::endl;*/
+    gain = p * h.t() * (h*p*h.t() + obsCovar).inv();
     
+    /*std::cout << "SMC: " << std::endl << tmp << std::endl;
+    gain = tmp;*/
+    
+    std::cout << "abc5" << std::endl;
     // update state matrix
+
     stateMatrix = (stateTrans*stateMatrix) + (gain*((h*stateMatrix + obsCovar)-h*stateMatrix));
 
+
+std::cout << "abc6" << std::endl;
     // new ball
     cv::Vec3f newBall( stateMatrix.at<double>(0, 0), stateMatrix.at<double>(0, 1), ball[2] );
-
+std::cout << "abc7" << std::endl;
     // update p for next time
     p = (cv::Mat::eye(4, 4, CV_64F) - gain*h)*p;
-
+std::cout << "abc8" << std::endl;
     return newBall;
 }
 
+int stateSize = 4;
+int measSize = 2;
+int contrSize = 0;
+unsigned int type = CV_64F;
+cv::KalmanFilter kf(stateSize, measSize, contrSize, type);
+cv::Mat state(stateSize, 1, type);
+cv::Mat meas(measSize, 1, type);
 
+
+cv::Vec3f kalman( cv::Vec3f ball, std::chrono::microseconds delta){
+
+    std::cout << "delta: " << delta.count() << std::endl;
+
+    kf.transitionMatrix.at<double>(0, 1) = delta.count();
+    kf.transitionMatrix.at<double>(2, 3) = delta.count();
+
+    state = kf.predict();
+
+    meas.at<double>(0) = ball[0];
+    meas.at<double>(1) = ball[1];
+    
+    kf.correct(meas);
+    
+
+
+    cv::Vec3f newBall( state.at<double>(0, 0), state.at<double>(2,0), 8 );
+    return newBall;
+}
 
 
 
@@ -85,14 +132,31 @@ bool foundBall = false;
 
 void mouseHandler(int event,int x,int y, int flags,void* param){
     if( event == cv::EVENT_LBUTTONDOWN ){
-        cv::Vec3f ball(x, y, 8);
-        kalman(ball, std::chrono::microseconds(0));
+
+        state.at<double>(0) = x;
+        state.at<double>(1) = 0;
+        state.at<double>(2) = y;
+        state.at<double>(3) = 0;
+        
+        kf.statePost = state;
+        
+        foundBall = true;
     }
 }
 
 
 
 int main( int argc, char** argv ){
+    cv::setIdentity(kf.transitionMatrix);
+    kf.measurementMatrix = h;// = cv::Mat::zeros(measSize, stateSize, type);
+    kf.processNoiseCov = procCovar;
+    kf.measurementNoiseCov = obsCovar;
+    kf.errorCovPre = p;
+
+
+
+
+
     ImageProcessor proc;
     CameraCapture cam;
     while( !cam.init() );
@@ -150,6 +214,7 @@ int main( int argc, char** argv ){
             // get kalman ball
             if( foundBall ){
                 ball = kalman(ball, cam.captureDelta);
+                //std::cout << ball << std::endl;
             }
 
             cv::circle(imgOg, cv::Point((int)(ball)[0], (int)(ball)[1]), (int)(ball)[2], col, 3);
--- main/main.h
+++ main/main.h
@@ -1,3 +1,4 @@
 #include <iostream>
+#include <opencv2/video/tracking.hpp>
 #include "CameraCapture/CameraCapture.h"
 #include "ImageProcessing/ImageProcessor.h"
